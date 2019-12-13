28260322d888a48fc1de7170c9b9a1f562b27ea7
bungo matsumoto <matsumotobg@gmail.com>
2019-12-13 13:55:59 +0900

    rails g (devise right_holders, devise:controllers right_holders, devise:views right_holders

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_035930) do
+ActiveRecord::Schema.define(version: 2019_12_13_044436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "screen_name"
+    t.string "country"
+    t.string "city"
+    t.text "intro"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_clients_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+  end
 
   create_table "films", force: :cascade do |t|
     t.string "title", default: "", null: false
