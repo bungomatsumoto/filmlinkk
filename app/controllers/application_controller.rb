03d49a97312765158f59b612a652c88f992fcf12
bungo matsumoto <matsumotobg@gmail.com>
2019-12-13 15:47:45 +0900

    devise routing, views/tops

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    if resource_class == Client
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :screen_name, :country, :city, :intro, :icon])
+    elsif resource_class == RightHolder
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :intro, :icon])
+    else
+      super
+    end
+  end
 end
