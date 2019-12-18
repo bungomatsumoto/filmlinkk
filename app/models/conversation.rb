d15170ace045144700d84f9ad28dd639484925ec
bungo matsumoto <matsumotobg@gmail.com>
2019-12-18 15:38:08 +0900

    rails g controller Messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,12 @@
 class Conversation < ApplicationRecord
   belongs_to :client
   belongs_to :right_holder
-
   has_many :messages, dependent: :destroy
 
-  validates_uniqueness_of :client_id, scope: :rright_holder_id
+  scope :between, -> (right_holder_id, client_id) do
+    where(["(conversations.client_id = ? AND conversations.right_holder_id = ?)", client_id, right_holder_id])
+  end
+
+  validates_uniqueness_of :client_id, scope: :right_holder_id
 
 end
