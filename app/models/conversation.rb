6b21e5243a29144e15ae7ff87e90d28c598e2c30
bungo matsumoto <matsumotobg@gmail.com>
2019-12-23 11:26:43 +0900

    create chat

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,7 +4,7 @@ class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
 
   scope :between, -> (right_holder_id, client_id) do
-    where(["(conversations.client_id = ? AND conversations.right_holder_id = ?)", client_id, right_holder_id])
+    where(["(conversations.client_id = ? AND conversations.right_holder_id = ?) OR (conversations.client_id = ? AND conversations.right_holder_id = ?)", client_id, right_holder_id, right_holder_id, client_id])
   end
 
   # validates_uniqueness_of :client_id, scope: :right_holder_id
