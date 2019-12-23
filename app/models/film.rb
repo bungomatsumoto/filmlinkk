39845035832b5ad74384f70ff92282c2f6db2f5a
bungo matsumoto <matsumotobg@gmail.com>
2019-12-23 13:51:20 +0900

    implement ransack

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,5 +3,5 @@ class Film < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :booking_clients, through: :bookings, source: :client
   belongs_to :right_holder, optional: true
-  mount_uploaders :images, ImageUploader
+  mount_uploader :image, ImageUploader
 end
