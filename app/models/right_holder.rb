936253fedcd02dd63e7505dd022e2ce984ac874c
bungo matsumoto <matsumotobg@gmail.com>
2019-12-23 18:09:58 +0900

    films index table layout

--- app/models/right_holder.rb
+++ app/models/right_holder.rb
@@ -2,5 +2,9 @@ class RightHolder < ApplicationRecord
   has_many :films
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, length: { maximum: 50 }
+
   mount_uploader :icon, ImageUploader
 end
