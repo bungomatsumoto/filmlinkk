f95e5629836f877f2e904f04ee43647b6fdde9e3
bungo matsumoto <matsumotobg@gmail.com>
2019-12-24 22:00:48 +0900

    to heroku

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :films
   has_many :bookings, dependent: :destroy
