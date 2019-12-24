7a742a8e1b60d11ea787b59e9bc129d200fed439
bungo matsumoto <matsumotobg@gmail.com>
2019-12-24 22:00:48 +0900

    to heroku

--- app/models/right_holder.rb
+++ app/models/right_holder.rb
@@ -1,7 +1,7 @@
 class RightHolder < ApplicationRecord
   has_many :films
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
   validates :email, length: { maximum: 50 }
