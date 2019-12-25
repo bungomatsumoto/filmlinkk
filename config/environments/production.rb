1002288444bb71a2376b3a650c143dbba8932551
bungo matsumoto <matsumotobg@gmail.com>
2019-12-25 10:46:14 +0900

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: https://ancient-tor-72127.herokuapp.com/ }
+  # config.action_mailer.default_url_options = { host: https://ancient-tor-72127.herokuapp.com/ }
   config.action_mailer.delivery_method = :letter_opener_web
 
   # Ignore bad email addresses and do not raise email delivery errors.
